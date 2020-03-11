e56a2c07fd280601009b81a958f24e80653d9f72
florian <florianernst59@gmail.com>
2020-03-11 17:29:17 +0100

    Add a Message class with a JSON serializer

--- drone-simulator/build.sbt
+++ drone-simulator/build.sbt
@@ -3,11 +3,12 @@ name := "Spark Project"
 version := "1.0"
 
 
+import sbt._
+import Keys._
+
+
 scalaVersion := "2.11.8"
 
-
-libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind" % "2.6.5"
-libraryDependencies += "com.google.code.gson" % "gson" % "2.7"
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6"
-libraryDependencies += "org.apache.kafka" %% "kafka" % "2.1.0"
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.7.4"
+libraryDependencies += "org.apache.kafka" %% "kafka" % "2.4.0"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime
--- drone-simulator/src/main/scala/project/DroneSim.scala
+++ drone-simulator/src/main/scala/project/DroneSim.scala
@@ -1,9 +1,13 @@
 package project
 
 import java.util.Properties
+
 import org.apache.kafka.clients.producer._
+import play.api.libs.json._
 
 object DroneSim {
+  implicit val residentWrites: OWrites[Message] = Json.writes[Message]
+
   def main(args: Array[String]): Unit = {
     println("Sending test message")
 
@@ -15,20 +19,17 @@ object DroneSim {
     val producer = new KafkaProducer[String, String](props)
     val topic = "test"
 
-    writeToKafka(producer, topic, "drone-simulation-message", "first message")
-    writeToKafka(producer, topic, "drone-simulation-message", "second message")
-    writeToKafka(producer, topic, "drone-simulation-message", "third message")
-    writeToKafka(producer, topic, "drone-simulation-message", "fourth message")
-
-    println("Transmission over")
-  }
-  def writeToKafka(producer: KafkaProducer[String, String], topic: String, key: String, message: String): Unit = {
-    val record = new ProducerRecord[String, String](topic, "key", "coucou")
-
-    producer.send(record)
+    writeToKafka(producer, topic, Message("94800-Villejuif", "12:00", "100"))
+    writeToKafka(producer, topic, Message("94800-Villejuif", "12:05", "102", Some("99"), Some("5")))
 
     producer.close()
+    println("Transmission over")
+  }
 
+  def writeToKafka(producer: KafkaProducer[String, String], topic: String, message: Message): Unit = {
+    val record = new ProducerRecord[String, String](topic, Json.toJson(message).toString())
+
+    producer.send(record)
   }
 
 }
