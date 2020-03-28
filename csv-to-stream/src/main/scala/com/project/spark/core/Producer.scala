7255d25d96a0fa958023c4838fa874bd37bcb6e4
LeanaNeuber <LeanaNeuber@github.com>
2020-03-28 11:55:21 +0100

    Added cmd line parameters to csv to stream

--- csv-to-stream/src/main/scala/com/project/spark/core/Producer.scala
+++ csv-to-stream/src/main/scala/com/project/spark/core/Producer.scala
@@ -14,15 +14,19 @@ object Producer {
   implicit val residentWrites: OWrites[Message] = Json.writes[Message]
 
   def main(args: Array[String]): Unit = {
+    if (args.length < 3)
+      println("Three arguments are required to start this application: the aws region, the stream, and the path to the csv file!")
 
-    val pathToFile = "data/Parking_Violations_Issued_-_Fiscal_Year_2017.csv"
+    val region = args(0)
+    val stream = args(1)
+    val pathToFile = args(2)
 
-    val kinesisClient = buildKinesisClient(getCreds)
+    val kinesisClient = buildKinesisClient(region, getCreds)
 
-    extractCSV(pathToFile, kinesisClient)
+    extractCSV(stream, pathToFile, kinesisClient)
   }
 
-  private def extractCSV(pathToFile: String, kinesisClient: AmazonKinesis) = {
+  private def extractCSV(stream: String, pathToFile: String, kinesisClient: AmazonKinesis) = {
     val bufferedSource = Source.fromFile(pathToFile)
 
     val header = bufferedSource.getLines.next().split(",").toList
@@ -30,34 +34,34 @@ object Producer {
     bufferedSource
       .getLines()
       .drop(1)
-      .foreach(line => extractLines(kinesisClient, header, line.split(",").toList))
+      .foreach(line => extractLines(stream: String, kinesisClient, header, line.split(",").toList))
 
     bufferedSource.close()
   }
 
   //map to schema (case 48 columns do smth, case 51 do something, case else drop the line ) --> case class wtf?
-  private def extractLines(kinesisClient: AmazonKinesis, header: List[String], lineList: List[String]) = lineList match {
+  private def extractLines(stream: String, kinesisClient: AmazonKinesis, header: List[String], lineList: List[String]) = lineList match {
       case x if lineList.size > header.indexOf("Street Name") => {
         val location = x(header.indexOf("House Number")) + " " + x(header.indexOf("Street Name"))
         val time = x(header.indexOf("Issue Date"))
         val violationCode = x(header.indexOf("Violation Code"))
-        writeToKinesis(kinesisClient, "test", Message(location, time, scala.util.Random.nextInt(100000).toString, Some(violationCode)))
+        writeToKinesis(stream, kinesisClient, "test", Message(location, time, scala.util.Random.nextInt(100000).toString, Some(violationCode)))
       }
       case _ =>
   }
 
-  def writeToKinesis(kinesisClient: AmazonKinesis, topic: String, message: Message): Unit = {
+  def writeToKinesis(stream: String, kinesisClient: AmazonKinesis, topic: String, message: Message): Unit = {
     val putRecordRequest = new PutRecordRequest
-    putRecordRequest.setStreamName("prestacop")
+    putRecordRequest.setStreamName(stream)
     putRecordRequest.setPartitionKey("key")
     putRecordRequest.setData(ByteBuffer.wrap(Json.toJson(message).toString().getBytes()))
 
     kinesisClient.putRecord(putRecordRequest)
   }
 
-  private def buildKinesisClient(credentialsProvider: AWSCredentialsProvider) = {
+  private def buildKinesisClient(region: String, credentialsProvider: AWSCredentialsProvider) = {
     val clientBuilder = AmazonKinesisClientBuilder.standard()
-    clientBuilder.setRegion("eu-central-1")
+    clientBuilder.setRegion(region)
     clientBuilder.setCredentials(credentialsProvider)
     clientBuilder.build
   }
