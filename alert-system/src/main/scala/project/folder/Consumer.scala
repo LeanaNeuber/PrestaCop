3f93fa9d8dd2a9e1da9564199785aefdd0d1a4d9
LeanaNeuber <LeanaNeuber@github.com>
2020-03-29 23:15:54 +0200

    Push alerts to sns and normal data to prestacop dynamodb

--- alert-system/src/main/scala/project/folder/Consumer.scala
+++ alert-system/src/main/scala/project/folder/Consumer.scala
@@ -11,6 +11,8 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.amazonaws.services.dynamodbv2.document.Table
 import com.amazonaws.services.kinesis.model.{GetRecordsRequest, Record}
 import com.amazonaws.services.kinesis.{AmazonKinesis, AmazonKinesisClientBuilder}
+import com.amazonaws.services.sns.{AmazonSNS, AmazonSNSClientBuilder}
+import com.amazonaws.services.sns.model.PublishRequest
 import play.api.libs.json._
 
 import scala.jdk.CollectionConverters._
@@ -23,55 +25,49 @@ object Consumer {
 
   def main(args: Array[String]): Unit = {
     if (args.length < 3)
-      println("Three arguments are required to start this application: the aws region, the stream, and the dynamodb table name!")
+      println("Three arguments are required to start this application: the aws region, the stream, and the sns topic name!")
 
     val region = args(0)
     val stream = args(1)
-    val table = args(2)
+    val topicArn = args(2)
 
 
     val creds = getCreds
-    checkShard(stream, getDynamoTable(region, table, creds), buildKinesisClient(region, creds), "shardId-000000000000")
+    checkShard(buildSnsClient(region, creds), topicArn, stream, buildKinesisClient(region, creds), "shardId-000000000000")
   }
 
-  def checkShard(stream: String, table: Table, kinesisClient: AmazonKinesis, shardId: String): Unit = {
+  def checkShard(snsClient: AmazonSNS, topicArn: String, stream: String, kinesisClient: AmazonKinesis, shardId: String): Unit = {
     val shardIterator = kinesisClient.getShardIterator(stream, shardId, "LATEST").getShardIterator
-    loopPoll(table, kinesisClient, shardIterator)
+    loopPoll(snsClient, topicArn, kinesisClient, shardIterator)
   }
 
-  def loopPoll(table: Table, kinesisClient: AmazonKinesis, shardIterator: String):Unit =  {
+  def loopPoll( snsClient: AmazonSNS, topicArn: String, kinesisClient: AmazonKinesis, shardIterator: String):Unit =  {
 
     val getRecordsRequest = new GetRecordsRequest
     getRecordsRequest.setShardIterator(shardIterator)
     getRecordsRequest.setLimit(25)
 
     val recordsResult = kinesisClient.getRecords(getRecordsRequest)
-    processRecords(table, recordsResult.getRecords.asScala.toList)
+    processRecords(snsClient, topicArn,recordsResult.getRecords.asScala.toList)
 
     //Sleep for 1 seconds, then call the method again. Maybe not the best approach because never ending..
     Thread.sleep(1000)
 
     val next_iter = recordsResult.getNextShardIterator
-    loopPoll(table, kinesisClient, next_iter)
+    loopPoll(snsClient, topicArn, kinesisClient, next_iter)
 
   }
 
   // Raises an alarm if a message contains violationCode 666!
-  def processRecords(table: Table, records: List[Record]) = {
+  def processRecords(snsClient: AmazonSNS, topicArn: String, records: List[Record]) = {
     records.map(r => StandardCharsets.UTF_8.decode(r.getData).toString()).map(message => Json.parse(message).as[Message])
       .filter(message => message.violationCode.get.equals("666"))
-      .foreach(m => storeToDynamo(table, m))
+      .foreach(m => sendToSns(snsClient, topicArn, m))
   }
 
-  def storeToDynamo(table: Table, message: Message): Unit = {
-    table.putItem(new Item()
-      .withPrimaryKey("id", message.droneId+"_"+message.time)
-      .withString("location",message.location)
-      .withString("time",message.time)
-      .withString("droneId", message.droneId)
-      .withString("violationCode",message.violationCode.getOrElse(null))
-      .withString("violationImageId",message.violationImageId.getOrElse(null)))
+  def sendToSns(snsClient: AmazonSNS, topicArn: String, message: Message): Unit = {
 
+    snsClient.publish(new PublishRequest(topicArn, message.toString))
 
     println("----------------------------------------------------------------")
     println("ALERT!!!! For drone " + message.toString)
@@ -89,18 +85,15 @@ object Consumer {
     }
   }
 
-  private def getDynamoTable(region: String, table: String, credentialsProvider: AWSCredentialsProvider) = {
-    val clientBuilder = AmazonDynamoDBClientBuilder.standard()
-    clientBuilder.setRegion(region)
-    clientBuilder.setCredentials(credentialsProvider)
-    val client = clientBuilder.build
-
-    val dynamoDB = new DynamoDB(client)
-    dynamoDB.getTable(table)
-  }
-
   private def buildKinesisClient(region: String, credentialsProvider: AWSCredentialsProvider) = {
     val clientBuilder = AmazonKinesisClientBuilder.standard()
+    clientBuilder.setRegion(region)
+    clientBuilder.setCredentials(credentialsProvider)
+    clientBuilder.build
+  }
+
+  private def buildSnsClient(region: String, credentialsProvider: AWSCredentialsProvider) = {
+    val clientBuilder = AmazonSNSClientBuilder.standard()
     clientBuilder.setRegion(region)
     clientBuilder.setCredentials(credentialsProvider)
     clientBuilder.build
--- stream-to-storage/src/main/scala/project/blub/Consumer.scala
+++ stream-to-storage/src/main/scala/project/blub/Consumer.scala
@@ -4,9 +4,10 @@ import java.io.File
 import java.nio.charset.StandardCharsets
 
 import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider, PropertiesCredentials}
-import com.amazonaws.services.kinesis.model.{GetRecordsRequest, GetShardIteratorResult, Record}
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Item, Table}
+import com.amazonaws.services.kinesis.model.{GetRecordsRequest, Record}
 import com.amazonaws.services.kinesis.{AmazonKinesis, AmazonKinesisClientBuilder}
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import play.api.libs.json._
 import project.Message
 
@@ -21,23 +22,23 @@ object Consumer {
   def main(args: Array[String]): Unit = {
 
     if (args.length < 3)
-      println("Three arguments are required to start this application: the aws region, the stream, and the bucket name!")
+      println("Three arguments are required to start this application: the aws region, the stream, and the DynamoDb table!")
 
     val region = args(0)
     val stream = args(1)
-    val bucket = args(2)
+    val table = args(2)
 
     val creds = getCreds
-    checkShard(stream, bucket, buildS3Client(region, creds),buildKinesisClient(region, creds), "shardId-000000000000")
+    checkShard(stream, getDynamoTable(region, table, creds),buildKinesisClient(region, creds), "shardId-000000000000")
 
   }
 
-  def checkShard(stream: String, bucket: String, s3Client: AmazonS3, kinesisClient: AmazonKinesis, shardId: String): Unit = {
+  def checkShard(stream: String, table: Table, kinesisClient: AmazonKinesis, shardId: String): Unit = {
     val shardIterator = kinesisClient.getShardIterator(stream, shardId, "LATEST").getShardIterator
-    loopPoll(bucket, s3Client, kinesisClient, shardIterator)
+    loopPoll(table, kinesisClient, shardIterator)
   }
 
-  def loopPoll(bucket: String, s3Client: AmazonS3, kinesisClient: AmazonKinesis, shardIterator: String):Unit =  {
+  def loopPoll(table: Table, kinesisClient: AmazonKinesis, shardIterator: String):Unit =  {
 
     val getRecordsRequest = new GetRecordsRequest
     getRecordsRequest.setShardIterator(shardIterator)
@@ -45,23 +46,31 @@ object Consumer {
 
     val recordsResult = kinesisClient.getRecords(getRecordsRequest)
 
-    processRecords(bucket, s3Client, recordsResult.getRecords.asScala.toList)
+    processRecords(table, recordsResult.getRecords.asScala.toList)
 
     //Sleep for 1 seconds, then call the method again. Maybe not the best approach because never ending..
     Thread.sleep(1000)
 
     val next_iter = recordsResult.getNextShardIterator
-    loopPoll(bucket, s3Client,kinesisClient, next_iter)
+    loopPoll(table,kinesisClient, next_iter)
 
   }
 
-  def processRecords(bucket: String, s3Client: AmazonS3, records: List[Record]) = {
+  def processRecords(table: Table, records: List[Record]) = {
     records.map(r => StandardCharsets.UTF_8.decode(r.getData).toString()).map(message => Json.parse(message).as[Message])
-      .foreach(m => store(bucket, s3Client, m))
+      .foreach(m => storeToDynamo(table, m))
   }
 
-  def store(bucket: String, s3Client: AmazonS3, message: Message): Unit = {
-    s3Client.putObject(bucket, message.droneId +"_"+ message.time.replace("/", "-"), message.toString)
+  def storeToDynamo(table: Table, message: Message): Unit = {
+    table.putItem(new Item()
+      .withPrimaryKey("id", message.droneId+"_"+message.time)
+      .withString("location",message.location)
+      .withString("time",message.time)
+      .withString("droneId", message.droneId)
+      .withString("violationCode",message.violationCode.getOrElse(null))
+      .withString("violationImageId",message.violationImageId.getOrElse(null))
+      //Add a TTL of 5 days from now
+      .withLong("ttl", System.currentTimeMillis() / 1000L + 432000L  ))
   }
 
   private def getCreds = {
@@ -82,10 +91,14 @@ object Consumer {
     clientBuilder.build
   }
 
-  private def buildS3Client(region: String, credentialsProvider: AWSCredentialsProvider) = {
-    val clientBuilder = AmazonS3ClientBuilder.standard()
+  private def getDynamoTable(region: String, table: String, credentialsProvider: AWSCredentialsProvider) = {
+    val clientBuilder = AmazonDynamoDBClientBuilder.standard()
     clientBuilder.setRegion(region)
     clientBuilder.setCredentials(credentialsProvider)
-    clientBuilder.build
+    val client = clientBuilder.build
+
+    val dynamoDB = new DynamoDB(client)
+    dynamoDB.getTable(table)
   }
+
 }
