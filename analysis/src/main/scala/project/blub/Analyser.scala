9643f45951a3fc961d01797a02d65324b90a434c
LeanaNeuber <LeanaNeuber@github.com>
2020-03-31 23:54:05 +0200

    Attempt to read data from dynamodb

--- analysis/src/main/scala/project/blub/Analyser.scala
+++ analysis/src/main/scala/project/blub/Analyser.scala
@@ -13,11 +13,17 @@ import com.amazonaws.services.kinesis.{AmazonKinesis, AmazonKinesisClientBuilder
 import com.amazonaws.services.s3.model.{GetObjectRequest, ListObjectsRequest}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.amazonaws.services.simpledb.model.Item
+import org.apache.hadoop.dynamodb.DynamoDBItemWritable
+import org.apache.hadoop.mapred.JobConf
 import play.api.libs.json._
 import project.Message
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.hadoop.dynamodb.read.DynamoDBInputFormat
 
-import scala.jdk.CollectionConverters._
-
+import org.apache.hadoop.io.Text
 
 
 object Analyser {
@@ -31,7 +37,28 @@ object Analyser {
     val credits = getCreds
     val DBCLient = buildDynamoDBClient(region, table, credits)
 
-    val scanRequest = new ScanRequest()
+    val conf = new SparkConf()
+      .setAppName("Analyser")
+      .setMaster("local[*]") // here local mode. And * means you will use as much as you have cores.
+
+    val sc = SparkContext.getOrCreate(conf)
+
+    var jobConf = new JobConf(sc.hadoopConfiguration)
+    jobConf.set("dynamodb.servicename", "dynamodb")
+    jobConf.set("dynamodb.input.tableName", table)
+    jobConf.set("dynamodb.regionid", region)
+    jobConf.set("dynamodb.awsAccessKeyId", credits.getCredentials.getAWSAccessKeyId)
+    jobConf.set("dynamodb.awsSecretAccessKey", credits.getCredentials.getAWSSecretKey)
+    jobConf.set("mapred.output.format.class", "org.apache.hadoop.dynamodb.write.DynamoDBOutputFormat")
+    jobConf.set("mapred.input.format.class", "org.apache.hadoop.dynamodb.read.DynamoDBInputFormat")
+
+
+    var orders = sc.hadoopRDD(jobConf, classOf[DynamoDBInputFormat], classOf[Text], classOf[DynamoDBItemWritable])
+
+    orders.map(t => t._2.getItem()).collect.foreach(println)
+    val simple2: RDD[(String)] = orders.map { case (text, dbwritable) => (dbwritable.toString)}
+
+    /*val scanRequest = new ScanRequest()
       .withTableName(table);
     val result = DBCLient.scan(scanRequest)
 
@@ -43,7 +70,7 @@ object Analyser {
 
     val aaaah = result.getItems
     print(aaaah.getClass)
-
+  */
 
 
   }
